b68bdedb86bb5fd55823c0416da5d990898841e1
Miguel Ángel Ávila Rosas <miguelangelavilarosas1@gmail.com>
2025-11-25 11:34:37 +0100

    Credits Update (25/11/2025)

--- Assets/Scripts/EndGameManager.cs
+++ Assets/Scripts/EndGameManager.cs
@@ -6,6 +6,8 @@ public class EndGameManager : MonoBehaviour
 {
     [SerializeField]
     private TextMeshProUGUI textResult;
+    [SerializeField] GameObject MenuPanel;
+    [SerializeField] GameObject creditsPanel;
 
     private void Start()
     {
@@ -23,5 +25,32 @@ public class EndGameManager : MonoBehaviour
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void ShowCredits()
+    {
+        MenuPanel.SetActive(false);
+        creditsPanel.SetActive(true);
+    }
+
+    public void GoBackToMenu()
+    {
+        creditsPanel.SetActive(false);
+        MenuPanel.SetActive(true);
+    }
+
+    public void YoutubeLink()
+    {
+        Application.OpenURL("https://www.youtube.com/@bruxter1677");
+    }
+
+    public void GithubLink()
+    {
+        Application.OpenURL("https://github.com/MAARBruxter");
+    }
+
+    public void LinkedinLink()
+    {
+        Application.OpenURL("https://www.linkedin.com/in/miguel-%C3%A1ngel-%C3%A1vila-rosas-3b1371238/");
     }
 }
